af516d4b9f2de8bef3e7a52f68d9c7fa2910185d
FangBing <15110248007@139.com>
2020-03-26 19:34:22 +0800

    .

---
+++ 006-3BroadcastBestPractice/app/src/main/java/com/moyu/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,68 @@
+package com.moyu.broadcastbestpractice;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.moyu.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("warning");
+            builder.setMessage("you are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent i =new Intent(context,LoginActivity.class);
+                    context.startActivity(i);
+                }
+            });
+            builder.show();
+        }
+
+    }
+}
